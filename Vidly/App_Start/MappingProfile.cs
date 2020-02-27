b06b73c34a351c58a672ca143149a9943abf040c
Ethan Behar <behar.ethan@gmail.com>
2020-02-26 19:48:57 -0500

    adding ignore to Id for movie and customer in Mapping

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -9,8 +9,8 @@ namespace Vidly.App_Start
         public MappingProfile()
         {
             // Domain to Dto
-            Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
             
             // Dto to Domain
             Mapper.CreateMap<CustomerDto, Customer>();
