6a690e1c098b55e433506668620382b7129d2f6c
Ethan Behar <behar.ethan@gmail.com>
2020-03-03 14:34:43 -0500

    Displaying discount rate for customer at rental screen

--- Vidly/Dtos/MembershipTypeDto.cs
+++ Vidly/Dtos/MembershipTypeDto.cs
@@ -3,6 +3,10 @@
     public class MembershipTypeDto
     {
         public byte Id { get; set; }
+
         public string Name { get; set; }
+
+        public byte DiscountRate { get; set; }
+
     }
 }
