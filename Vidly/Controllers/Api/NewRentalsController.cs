32f4438f5d903048261a5bef43de83fcba4ce80d
Ethan Behar <behar.ethan@gmail.com>
2020-03-02 21:46:00 -0500

    Added toastr for toats notifacations when submitting new rental - client side validation on rental form

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -19,7 +19,7 @@ namespace Vidly.Controllers.Api
         // Optimistic approach is better for internal API
         // We can be assured 3 edge cases won't happen since it's internal
         [HttpPost]
-        public IHttpActionResult OptimisticCreateNewRental(NewRentalDto newRentalDto)
+        public IHttpActionResult CreateNewRental(NewRentalDto newRentalDto)
         {
             var customer = _context.Customers
                 .Single(c => c.Id == newRentalDto.CustomerId);
@@ -51,44 +51,44 @@ namespace Vidly.Controllers.Api
 
         // Defensive apporach is better for public APIs
         // All these edge validation checks make the API more stable
-        [HttpPost]
-        public IHttpActionResult DefensiveCreateNewRental(NewRentalDto newRentalDto)
-        {
-            if (newRentalDto.MovieIds.Count == 0)
-                return BadRequest("No Movie Ids have been given.");
+        //[HttpPost]
+        //public IHttpActionResult DefensiveCreateNewRental(NewRentalDto newRentalDto)
+        //{
+        //    if (newRentalDto.MovieIds.Count == 0)
+        //        return BadRequest("No Movie Ids have been given.");
 
-            var customer = _context.Customers
-                .SingleOrDefault(c => c.Id == newRentalDto.CustomerId);
+        //    var customer = _context.Customers
+        //        .SingleOrDefault(c => c.Id == newRentalDto.CustomerId);
 
-            if (customer == null)
-                return BadRequest("CustomerId is not valid.");
+        //    if (customer == null)
+        //        return BadRequest("CustomerId is not valid.");
 
-            var movies = _context.Movies
-                .Where(m => newRentalDto.MovieIds.Contains(m.Id)).ToList();
+        //    var movies = _context.Movies
+        //        .Where(m => newRentalDto.MovieIds.Contains(m.Id)).ToList();
 
-            if (movies.Count != newRentalDto.MovieIds.Count)
-                return BadRequest("One or more MovieIds are invalid.");
+        //    if (movies.Count != newRentalDto.MovieIds.Count)
+        //        return BadRequest("One or more MovieIds are invalid.");
 
-            foreach (Movie m in movies)
-            {
-                if (m.NumberAvailable == 0)
-                    return BadRequest("Movie is not available.");
+        //    foreach (Movie m in movies)
+        //    {
+        //        if (m.NumberAvailable == 0)
+        //            return BadRequest("Movie is not available.");
 
-                m.NumberAvailable -= 1;
+        //        m.NumberAvailable -= 1;
                 
-                var rental = new Rental
-                {
-                    Customer = customer,
-                    Movie = m,
-                    DateRented = DateTime.Now
-                };
+        //        var rental = new Rental
+        //        {
+        //            Customer = customer,
+        //            Movie = m,
+        //            DateRented = DateTime.Now
+        //        };
 
-                _context.Rentals.Add(rental);
-            }
+        //        _context.Rentals.Add(rental);
+        //    }
 
-            _context.SaveChanges();
+        //    _context.SaveChanges();
 
-            return Ok();
-        }
+        //    return Ok();
+        //}
     }
 }
